c376ed8347f597a46d4816d74ddc78e6567f41e2
evelynleal84 <evelyn.designer@gmail.com>
2025-11-25 22:05:50 -0300

    Alteração Evelyn

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+//Teste com Nokia
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "IMEI123456", 16);
+nokia.Ligar();  
+nokia.ReceberLigacao();     
+nokia.InstalarAplicativo("WhatsApp");
+
+//Teste com Iphone
+Iphone iphone = new Iphone("987654321", "iPhone 13", "IMEI654321", 128);
+iphone.Ligar();     
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");     
+
